e9ff86871de4d094534cb6360bfd8cd0dc38b8fb
Your Name <your-email@github.com>
2026-04-17 17:03:53 -0500

    final changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
